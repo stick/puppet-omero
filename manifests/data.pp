214b333c1a220e792bb554df2a11bbb740b2a385
Chris MacLeod <ckm@glencoesoftware.com>
2012-03-14 12:45:28 +0000

    forgot add

---
+++ manifests/data.pp
@@ -0,0 +1,20 @@
+# Class: omero::data
+#
+# This class does stuff that you describe here
+#
+# Parameters:
+#   $parameter:
+#       this global variable is used to do things
+#
+# Actions:
+#   Actions should be described here
+#
+# Requires:
+#   - Package["foopackage"]
+#
+# Sample Usage:
+#
+class omero::data {
+  $omero_home = '/opt/omero'
+  $ice_home = '/usr/share/Ice-3.3.1'
+}
